160a79327df88724305fb2dd23060a47bd350069
manu <manurodgersgoat@gmail.com>
2018-05-13 14:56:52 +1000

    init

---
+++ src/app/main.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+
+import App from "./components/App";
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app-container")
+);
